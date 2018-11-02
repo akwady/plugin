f52b08c9098c9caf7b8179de4e9c5109ace3cc93
unknown <thevinh1841996@gmail.com>
2018-11-02 09:14:52 +0700

    updata

---
+++ loadding/header.php
@@ -0,0 +1,29 @@
+<div class="loader">
+	<h1>
+		Loadding
+	</h1>
+</div>
+
+
+<style>
+	.loader {
+		background-color: #c61633;
+		position: fixed;
+		height: 100vh;
+		width: 100vw;
+		top: 0;
+		left: 0;
+		z-index: 99999999;
+		display: -webkit-inline-box;
+		display: -ms-inline-flexbox;
+		display: inline-flex;
+		-webkit-box-align: center;
+		-ms-flex-align: center;
+		align-items: center;
+	}
+
+	.loader h1 {
+		width: 100%;
+		text-align: center;
+	}
+</style>
---
+++ loadding/style.js
@@ -0,0 +1,11 @@
+$(function () {
+
+    var $mainwindow = $(window);
+
+    $mainwindow.on('load', function () {
+
+        $(".loader").fadeOut();
+
+    });
+
+});
